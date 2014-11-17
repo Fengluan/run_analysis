94d2bc21e08c822a1b601c6996dc9d5352a8ab1c
Fengluan <693264936@qq.com>
2014-11-17 08:55:15 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,23 @@
 library(plyr)
-X_test <- read.table("D:/test/X_test.txt",header = FALSE)
-X_train <- read.table("D:/train/X_train.txt",header = FALSE)
-y_test <- read.table("D:/test/y_test.txt",header = FALSE)
-y_train <- read.table("D:/train/y_train.txt",header = FALSE)
-subject_test <- read.table("D:/test/subject_test.txt",header = FALSE)
-subject_train <- read.table("D:/train/subject_train.txt",header = FALSE)
-all_data <- cbind(rbind(y_test,y_train),rbind(subject_test,subject_train))
-all_data <- cbind(all_data,rbind(X_test,X_train))
-all_mean <- apply(all_data[,3:length(all_data)],2,mean)
-all_sd <- apply(all_data[,3:length(all_data)],2,sd)
-activity_labels <- read.table("C:/Users/Administrator/Desktop/Getting and Cleaning Data/UCI HAR Dataset/activity_labels.txt",header = FALSE)
-names(all_data)[1:2] <- c("activity","subject")
-names(activity_labels)[1] <- "activity"
-all_data <- join(activity_labels, all_data, "activity")
-all_data <- all_data[,-1]
-names(all_data)[1] <- "activity"
-for(i in 3:length(all_data)){
-	x <- tapply(all_data[,i],all_data$activity,mean)
-	y <- tapply(all_data[,i],all_data$subject,mean)
-	if(i == 3){
-		activity_mean <- x
-		subject_mean <- y
-	}
-	else{
-		activity_mean <- rbind(activity_mean,x)
-		subject_mean <- rbind(subject_mean,y)
-	}
+X_test <- read.table(".../Getting and Cleaning Data/UCI HAR Dataset/test/X_test.txt")
+X_train <- read.table(".../Getting and Cleaning Data/UCI HAR Dataset/train/X_train.txt")
+y_test <- read.table(".../Getting and Cleaning Data/UCI HAR Dataset/test/y_test.txt")
+y_train <- read.table(".../Getting and Cleaning Data/UCI HAR Dataset/train/y_train.txt")
+activity_labels <- read.table(".../Getting and Cleaning Data/UCI HAR Dataset/activity_labels.txt")
+features <- read.table(".../Getting and Cleaning Data/UCI HAR Dataset/features.txt")
+n_mean <- grep("mean",features[,2])
+n_std <- grep("std",features[,2])
+n <- sort(c(n_mean, n_std))
+X_data <- rbind(X_test, X_train)
+colnames(X_data) <- features[,2]
+data_set <- X_data[, n]
+y_labels <- rbind(y_test, y_train)
+act_names <- join(y_labels, activity_labels, "V1")
+data_set <- cbind(activity-name = act_names[,2], data_set)
+aver_act <- apply(data_set[,2:length(data_set)],2,mean)
+aver_act <- c(activity = "all",aver_act)
+for(i in activity_labels[,2]){
+	aver <- apply(data_set[data_set[,1] == i,2:length(data_set)],2,mean)
+	aver <- c(activity = i,aver)
+	aver_act <- rbind(aver_act, aver)
 }
-rownames(activity_mean) <- NULL
-rownames(subject_mean) <- NULL
